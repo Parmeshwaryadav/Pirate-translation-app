e8ba0a201978cf636868aff8921a9129ef601f69
Parmeshwar yadav <yadavparmeshwar342@gmail.com>
2021-05-14 00:39:45 +0530

    text-Input

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var tansaltebutton = document.querySelector("#translate-button");
+
+var textInput = document.querySelector("#text-input")
+
+console.log(textInput);
+
 function clickHandler() {
     console.log("clicked");
+    console.log("input", textInput,)
 };
 tansaltebutton.addEventListener("click", clickHandler);
 
