19c078e42aac01d28f725b3ae14f4d8060884fa9
Parmeshwar yadav <yadavparmeshwar342@gmail.com>
2021-05-22 18:45:18 +0530

    translation text

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 var tansaltebutton = document.querySelector("#translate-button");
 var textInput = document.querySelector("#text-input");
 var output = document.querySelector("#text-output");
-console.log(output)
-// output.innerText = "Parmeshwar"
 
+var siteURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text) {
+    return siteURL + "?text=" + text;
+}
 function clickHandler() {
-    console.log("clicked");
-    console.log("input", textInput.value)
+    output.innerText = "api" + textInput.value;
 };
 tansaltebutton.addEventListener("click", clickHandler);
 
