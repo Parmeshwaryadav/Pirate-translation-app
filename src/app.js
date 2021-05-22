b965ec66df61f5d5e80f7d4716e9391a8b9b9c24
Parmeshwar yadav <yadavparmeshwar342@gmail.com>
2021-05-22 16:17:41 +0530

    Text-output

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 var tansaltebutton = document.querySelector("#translate-button");
-
-var textInput = document.querySelector("#text-input")
+var textInput = document.querySelector("#text-input");
+var output = document.querySelector("#text-output");
+console.log(output)
+// output.innerText = "Parmeshwar"
 
 function clickHandler() {
     console.log("clicked");
