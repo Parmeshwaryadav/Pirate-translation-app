eca5760c499ad10e1f857fc2cdc6aa95521ccb35
Parmeshwar yadav <yadavparmeshwar342@gmail.com>
2021-05-23 17:16:56 +0530

    comments

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ var tansaltebutton = document.querySelector("#translate-button");
 var textInput = document.querySelector("#text-input");
 var output = document.querySelector("#text-output");
 
+//Api from https://funtranslations.com/
 var siteURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+//
 function getTranslationURL(text) {
     return siteURL + "?text=" + text;
 };
+//error messages 
 function errorHandler(error) {
     console.log("Something Went Wrong On Server", error)
     alert("Something Went Wrong On Server Please try Again !")      
