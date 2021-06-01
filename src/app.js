e0fa26bbd75874e67d1112e64c62e87a2744c1fb
Parmeshwar yadav <yadavparmeshwar342@gmail.com>
2021-06-01 14:57:26 +0530

    add social media icons

--- src/app.js
+++ src/app.js
@@ -8,19 +8,20 @@ var siteURL = "https://api.funtranslations.com/translate/pirate.json"
 function getTranslationURL(text) {
     return siteURL + "?text=" + text;
 }
+
 function clickHandler() {
     var inputText = textInput.value;
-    fetch(getTranslationURL(inputText)) 
-    .then(response => response.json())
-    .then(json => {
-        var translatedInput = json.contents.translated;
-        output.innerText = translatedInput; 
-    })
-    .catch(errorHandler)  
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedInput = json.contents.translated;
+            output.innerText = translatedInput;
+        })
+        .catch(errorHandler)
 }
 tansaltebutton.addEventListener("click", clickHandler);
 //error messages 
 function errorHandler(error) {
     console.log("Something Went Wrong On Server", error)
-    alert("Something Went Wrong On Server Please try Again !")      
+    alert("Something Went Wrong On Server Please try Again !")
 };
